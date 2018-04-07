639d052c731fffe9107fcdee13ed16be038f6ada
rosstester <rosstester@users.noreply.github.com>
2018-04-07 13:37:36 +0200

    Usuniecie ilosci znakow z plikow

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Tomasz Przemyslaw"
+moje_imie = "Tomasz Przemo"
 msg = "Hello World!"
 
 
